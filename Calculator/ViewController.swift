c369f27713728b54c5de1d306ee1bf92e42f7d8f
Juan Felipe Zorrilla <juan19_93@hotmail.com>
2022-08-03 16:59:55 -0500

    Refactored code with computed properties

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,21 +11,68 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var acButton: UIButton!
     
+    private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Could not convert display label text to a Double")
+            }
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "+/-":
+                displayValue = displayValue * -1
+            case "AC":
+                displayLabel.text = "0"
+            case "C":
+                displayLabel.text = "0"
+                acButton.setTitle("AC", for: .normal)
+            case "%":
+                displayValue = displayValue / 100
+            default:
+                break
+            }
+        }
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber == true {
+                acButton.setTitle("C", for: .normal)
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text?.append(contentsOf: numValue)
+            }
+        }
     }
-
 }
 
